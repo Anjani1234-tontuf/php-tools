ad43ffbb4549323ec4d00ae03bc9a30ce423cd5e
Anjani-Kumar-Mishra <62552697+Anjani1234-tontuf@users.noreply.github.com>
2020-07-28 21:18:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
-function trace_router($host,$unix)
-{
-$host= preg_replace ("/[^A-Za-z0-9.]/","",$host);
-echo '<pre>';
-//check target IP or domain
-if ($unix)
-{
-system ("traceroute $host");
-system("killall -q traceroute");// kill all traceroute processes in case there are some stalled ones or use echo 'traceroute' to execute without shell
-}
-else
-{
-system("tracert $host");
-}
-echo '</pre>';
-}
-trace_router("hscripts.com",1);
-?>
+$color = "red";
+echo "Kirmada " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+?> 
+
+</body>
+</html>
